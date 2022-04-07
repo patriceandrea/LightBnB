ea4f28b1467504f85491f933501b4f2ccc5cdaaf
patriceandrea <andrea_pavico98@yahoo.ca>
2022-04-07 19:18:14 +0000

    refactors addProperty functions

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -25,7 +25,7 @@ const getUserWithEmail = function (email) {
     .query(`SELECT * FROM users where email = $1`, [email])
     .then(result => {
       if (result) {
-        // console.log(result.rows[0])
+        console.log(result.rows[0]);
         return result.rows[0];
       } else {
         return null;
@@ -108,6 +108,7 @@ const getAllReservations = function (guest_id, limit = 10) {
     LIMIT $2; `,
       [guest_id, limit])
     .then(result => {
+      console.log(result.rows);
       return result.rows;
     })
     .catch((err) => {
@@ -197,10 +198,39 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
+
+
+
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+
+  const queryString = ` 
+  INSERT INTO properties
+    (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night,street, city, province,post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+   RETURNING *;`;
+  const values = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms
+  ];
+
+  return pool.query(queryString, values)
+    .then(result => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.addProperty = addProperty;
