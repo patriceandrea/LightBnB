7932efad2767916f5053ac6a6de0fea442c94543
patriceandrea <andrea_pavico98@yahoo.ca>
2022-04-07 15:40:16 +0000

    refactors getUserWithEmail, getUserWithId, and addUser function with queries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,6 +1,7 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 const { Pool } = require('pg');
+const { password } = require('pg/lib/defaults');
 
 const pool = new Pool({
   user: 'vagrant',
@@ -20,16 +21,21 @@ pool.connect();
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  return pool
+    .query(`SELECT * FROM users where email = $1`, [email])
+    .then(result => {
+      if (result) {
+        // console.log(result.rows[0])
+        return result.rows[0];
+      } else {
+        return null;
+      }
+
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -39,7 +45,19 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  return pool
+    .query(`SELECT * FROM users where id = $1`, [id])
+    .then(result => {
+      if (result) {
+        return result.rows[0];
+      } else {
+        return null;
+      }
+
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.getUserWithId = getUserWithId;
 
@@ -49,11 +67,27 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
+
+//  INSERT INTO users
+//  (name, email, password)
+// VALUES
+//  ('Eva Stanley ', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.')
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool
+    .query(` INSERT INTO users
+       (name, email, password)
+        VALUES
+     ($1, $2, $3)
+     RETURNING *;`,
+      [user.name, user.email, user.password])
+    .then(result => {
+      return result.rows[0].id;
+
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+
 }
 exports.addUser = addUser;
 
